17a9ef7512aadf2ed49875c67cde52b48249215e
1997hikaru <hikarutakata0224@gmail.com>
2021-07-05 22:06:00 +0900

    userモデル修正（可読性向上のため）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-       validates :nickname
-       validates :last_name,            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-       validates :first_name,           format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-       validates :last_name_reading,    format: { with: /\A[ァ-ヶー－]+\z/ }
-       validates :first_name_reading,   format: { with: /\A[ァ-ヶー－]+\z/ }
-       validates :birthday
-       validates :password,             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :nickname
+    validates :birthday
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  
+      validates :last_name
+      validates :first_name_reading
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ }
+       validates :last_name_reading
+       validates :first_name_reading
+    end
   end
 end
