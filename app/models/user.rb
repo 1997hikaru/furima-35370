0768fd6cca7affcc959a488631a5bee48e15e161
1997hikaru <hikarutakata0224@gmail.com>
2021-08-08 12:22:01 +0900

    ユーザー登録のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
 
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       validates :last_name
-      validates :first_name_reading
+      validates :first_name
     end
 
     with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
